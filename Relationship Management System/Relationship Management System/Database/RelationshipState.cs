92f40c83a62759a90a1ed396a48d9132be0d26cf
Aaron Campf <aaroncampf@gmail.com>
2017-08-20 09:21:31 -0700

    Added stuff

---
+++ Relationship Management System/Relationship Management System/Database/RelationshipState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relationship_Management_System.Database {
+  /// <summary>
+  /// Not in database
+  /// </summary>
+  public enum RelationshipState {
+    New = 1,
+  }
+}

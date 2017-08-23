d78e3bda615a9ee20eff5e8a8c7941c022c35980
aaroncampf <aaroncampf@gmail.com>
2017-08-23 09:38:04 -0700

    Added more to SocialLink

--- Relationship Management System/Relationship Management System/Database/SocialLink.cs
+++ Relationship Management System/Relationship Management System/Database/SocialLink.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Relationship_Management_System.Database
-{
-	public class SocialLink
-	{
+namespace Relationship_Management_System.Database {
+	/// <summary>
+	/// Represents a person in your social network that could link you to potential contacts
+	/// </summary>
+	public class SocialLink {
 		public int Id { get; set; }
 		public string Name { get; set; }
+		public string Address { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string Zip { get; set; }
+		public string Profile { get; set; }
 		public string Details { get; set; }
 	}
 }

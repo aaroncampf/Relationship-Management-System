d83d88214c940c1fc0dd585f6320f212185ec767
aaroncampf <aaroncampf@gmail.com>
2017-09-13 09:46:51 -0700

    stuff

--- Relationship Management System/Relationship Management System/App.xaml.cs
+++ Relationship Management System/Relationship Management System/App.xaml.cs
@@ -15,11 +15,27 @@ namespace Relationship_Management_System {
 		protected override void OnStartup(StartupEventArgs e) {
 			base.OnStartup(e);
 
+			#region CefSharp.CefSettings
+			{ // <= this forces scoping for 0 pollution :D
+				CefSharp.CefSettings settings = new CefSharp.CefSettings();
+
+				if (!System.IO.Directory.Exists(System.IO.Directory.GetCurrentDirectory() + @"\CefSharp_Cache")) {
+					System.IO.Directory.Create(System.IO.Directory.GetCurrentDirectory() + @"\CefSharp_Cache");
+				}
+
+				settings.CachePath = System.IO.Directory.GetCurrentDirectory() + @"\CefSharp_Cache";
+
+
+
+				CefSharp.Cef.Initialize(settings);
+			}
+			#endregion
+
 			this.DispatcherUnhandledException += App_DispatcherUnhandledException;
 		}
 
 		void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e) {
 			MessageBox.Show(e.ToString(), e.GetType().FullName, MessageBoxButton.OK, MessageBoxImage.Error);
-		}		
+		}
 	}
 }
